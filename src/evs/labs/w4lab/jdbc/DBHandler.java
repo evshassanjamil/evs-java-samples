eff3e2fe9e5496aa5e70c7dd471ba8029a0cbb50
Muhammad Hassan Jamil <hassan.jamil@tplmaps.com>
2020-12-21 13:41:13 +0500

    - update JDBC self database creation implementations and samples

--- src/evs/labs/w4lab/jdbc/DBHandler.java
+++ src/evs/labs/w4lab/jdbc/DBHandler.java
@@ -8,14 +8,15 @@ import java.sql.*;
  */
 class DBHandler {
     // SQLite Connection String
-    private String url = "jdbc:sqlite:";
+    private String mPath = "jdbc:sqlite:";
 
     /**
-     * Constructor to initialize {@link DBHandler} object
-     * @param dbPath File System's absolute path e.g. <path>/filename.db
+     * Constructor to initialize {@link DBHandler}
+     *
+     * @param dbPath Db File System's absolute path e.g. ProjectAbsolutePath/db/filename.db
      */
     DBHandler(String dbPath) {
-        url += dbPath;
+        mPath += dbPath;
     }
 
     /**
@@ -26,10 +27,28 @@ class DBHandler {
     public Connection connect() {
         try {
             Class.forName("org.sqlite.JDBC");
-            return DriverManager.getConnection(url);
+            return DriverManager.getConnection(mPath);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    /**
+     * Creating database by doing a try establishing connection with DB File,
+     * Automatically creates new db file if not exists.
+     */
+    public void createNewDatabase() {
+        try (Connection conn = connect()) {
+            Class.forName("org.sqlite.JDBC");
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 
@@ -48,15 +67,18 @@ class DBHandler {
 
     /**
      * Create a new table in the database
-     * @param sql Create table SQLite query
+     *
+     * @param query Create table SQLite query
      */
-    public void createTable(String sql) {
+    public void createTable(String query) {
         Connection connection;
         try {
             connection = connect();
             Statement statement = connection.createStatement();
             // Creating table
-            statement.execute(sql);
+            boolean result = statement.execute(query);
+            if (result)
+                System.out.println("Table created successfully");
             // Closing connection
             closeConnection();
         } catch (SQLException e) {
@@ -66,15 +88,18 @@ class DBHandler {
 
     /**
      * Drop table from the test database
-     * @param sql Drop table SQLite query
+     *
+     * @param query Drop table SQLite query
      */
-    public void dropTable(String sql) {
+    public void dropTable(String query) {
         Connection connection;
         try {
             connection = connect();
             Statement statement = connection.createStatement();
             // Dropping table
-            statement.execute(sql);
+            boolean result = statement.execute(query);
+            if (result)
+                System.out.println("Table Dropped");
             // Closing connection
             closeConnection();
         } catch (SQLException e) {
@@ -84,14 +109,15 @@ class DBHandler {
 
     /**
      * Insert a new row into the product table
-     * @param sql
+     *
+     * @param query
      * @param values
      */
-    public void insert(String sql, String[] values) {
+    public void insert(String query, String[] values) {
 
         try {
             Connection connection = connect();
-            PreparedStatement statement = connection.prepareStatement(sql);
+            PreparedStatement statement = connection.prepareStatement(query);
             statement.setInt(1, Integer.valueOf(values[0]));
             statement.setString(2, values[1]);
             statement.setString(3, values[2]);
@@ -99,7 +125,7 @@ class DBHandler {
             statement.setString(5, values[4]);
             int result = statement.executeUpdate();
             if (result == 1)
-                System.out.println("Inserted Successfully");
+                System.out.println("Record Inserted");
             closeConnection();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -108,18 +134,21 @@ class DBHandler {
 
     /**
      * Update row data of product table specified by the id
-     * @param sql
-     * @param values
+     *
+     * @param query  SQL update query
+     * @param values sequenced String values array
      */
-    public void update(String sql, String[] values) {
+    public void update(String query, String[] values) {
         try {
             Connection connection = connect();
-            PreparedStatement statement = connection.prepareStatement(sql);
+            PreparedStatement statement = connection.prepareStatement(query);
             // set the corresponding param
             statement.setString(1, values[1]);
-            statement.setInt(2, Integer.valueOf(values[0]));
+            statement.setInt(2, Integer.parseInt(values[0]));
             // update
-            statement.executeUpdate();
+            int result = statement.executeUpdate();
+            if (result > 0)
+                System.out.println("Record Updated with id = " + values[0]);
             closeConnection();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -128,17 +157,20 @@ class DBHandler {
 
     /**
      * Delete a row of product table specified by the id
-     * @param sql Delete SQLite query
-     * @param id product id needs to be deleted
+     *
+     * @param query Delete SQLite query
+     * @param id    product id needs to be deleted
      */
-    public void delete(String sql, int id) {
+    public void delete(String query, int id) {
         try {
             Connection connection = connect();
-            PreparedStatement statement = connection.prepareStatement(sql);
+            PreparedStatement statement = connection.prepareStatement(query);
             // set the corresponding param
             statement.setInt(1, id);
             // execute the delete statement
-            statement.executeUpdate();
+            int result = statement.executeUpdate();
+            if (result > 0)
+                System.out.println("Record deleted with id = " + result);
             closeConnection();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -149,11 +181,11 @@ class DBHandler {
      * Select all rows in the table, make sure to call {@link DBHandler#closeConnection()}
      * after consuming the {@link ResultSet}.
      */
-    public ResultSet selectAll(String sql) {
+    public ResultSet select(String query) {
         try {
             Connection connection = connect();
             Statement statement = connection.createStatement();
-            return statement.executeQuery(sql);
+            return statement.executeQuery(query);
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
--- src/evs/labs/w4lab/jdbc/TestJDBC.java
+++ src/evs/labs/w4lab/jdbc/TestJDBC.java
@@ -1,55 +1,57 @@
 package evs.labs.w4lab.jdbc;
 
+import java.sql.ResultSet;
+
 public class TestJDBC {
-    public static void main(String[] args) {
+    public static void main(String[] args) {// CREATING DATABASE BY SETTING DB PATH
+        String dbFilePath = "E:\\Development\\Workspaces\\IntellijWorkspace\\evs\\evs-java-samples\\db\\ProductDb.db";
+        //String dbPath = "Project-Absolute-Path\\db\\ProductDb.db";
+        DBHandler dbHandler = new DBHandler(dbFilePath);
+        dbHandler.createNewDatabase();
+
+        ///////// CREATE TABLE ///////////
+        String create = "CREATE TABLE IF NOT EXISTS product (\n"
+                + "    id INTEGER PRIMARY KEY,\n"
+                + "    name TEXT NOT NULL,\n"
+                + "    category VARCHAR(30),\n"
+                + "    added_date DATETIME,\n"
+                + "    expiry_date DATETIME\n"
+                + ");";
+        dbHandler.createTable(create);
+
+
+        /////////// INSERT ///////////////
+        String insert = "INSERT INTO product(id, name, category, added_date,  expiry_date)  VALUES(?,?,?,?,?)";
+        String[] values1 = new String[]{"10", "Pepsi", "Beverages", "2020-12-21 13:06:30", "2020-12-27 13:06:30"};
+        dbHandler.insert(insert, values1);
+        String[] values2 = new String[]{"20", "Coca Cola", "Beverages", "2020-12-21 13:06:30", "2020-12-27 13:06:30"};
+        dbHandler.insert(insert, values2);
+
+
+        ////////////// SELECT ALL ///////////////
         try {
-            String dbPath = "E:\\Development\\Workspaces\\IntellijWorkspace\\SampleJavaProject\\db\\main.db";
-            DBHandler db = new DBHandler(dbPath);
-            ////////////// DROP TABLE ///////////////
-            /*String drop = "DROP TABLE product";
-            db.dropTable(drop);*/
-
-            ////////////// CREATE TABLE ///////////////
-            String create = "CREATE TABLE IF NOT EXISTS product (\n"
-                    + "    id INTEGER PRIMARY KEY,\n"
-                    + "    name TEXT NOT NULL,\n"
-                    + "    category VARCHAR(30),\n"
-                    + "    added_date DATETIME,\n"
-                    + "    expiry_date DATETIME\n"
-                    + ");";
-            db.createTable(create);
-
-            ////////////// INSERT ROW ///////////////
-            /*String insert = "INSERT INTO product(id, name, category, added_date, expiry_date) " +
-                    " VALUES(?,?,?,?,?)";
-            String[] insertValues = new String[]{"1", "Pepsi", "Beverage", "2017-12-10 19:34:00",
-                    "2018-12-10 19:34:00"};
-            db.insert(insert, insertValues);*/
-
-            ////////////// UPDATE ROW ///////////////
-            /*String update = "UPDATE product "
-                    + "SET name = ? "
-                    + "WHERE id = ?";
-            String[] updateValues = new String[]{"1", "Slice"};
-            db.update(update, updateValues);*/
-
-            ////////////// DELETE ROW ///////////////
-            /*String delete = "DELETE FROM product WHERE id = ?";
-            db.delete(delete, 1);*/
-
-            ////////////// SELECT ROWS ///////////////
-            /*ResultSet resultSet = db.selectAll("SELECT * FROM product");
+            ResultSet resultSet = dbHandler.select("SELECT * FROM product");
             while (resultSet.next()) {
                 System.out.println(resultSet.getInt("id") + "\t" +
                         resultSet.getString("name") + "\t" +
                         resultSet.getString("category") + "\t" +
-                        resultSet.getString("added_date") + "\t" +
+                        resultSet.getString("added_date") + "\t\t" +
                         resultSet.getString("expiry_date"));
             }
-            db.closeConnection();*/
-
+            dbHandler.closeConnection();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        ////////////// UPDATE ROW ///////////////
+        /*String update = "UPDATE product "
+                + "SET name = ? "
+                + "WHERE id = ?";
+        String[] updateValues = new String[]{"10", "Slice Mango"};
+        dbHandler.update(update, updateValues);*/
+
+        ////////////// DELETE ROW ///////////////
+        /*String delete = "DELETE FROM product WHERE id = ?";
+        dbHandler.delete(delete, 10);*/
     }
 }
